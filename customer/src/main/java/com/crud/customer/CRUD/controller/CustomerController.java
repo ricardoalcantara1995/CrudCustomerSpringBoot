8392e6566cddd48d7daef92f82e1d2479210b8c8
Ricardo Alcantara <alcantararicardo04@gmail.com>
2024-10-02 09:48:14 -0500

    CustomerController: Se agrega metodo que retorna todos los clientes

--- customer/src/main/java/com/crud/customer/CRUD/controller/CustomerController.java
+++ customer/src/main/java/com/crud/customer/CRUD/controller/CustomerController.java
@@ -2,15 +2,26 @@ package com.crud.customer.CRUD.controller;
 
 import com.crud.customer.CRUD.model.Customer;
 import com.crud.customer.CRUD.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.net.http.HttpResponse;
 import java.util.List;
 
-
+@RestController
+@RequestMapping("/customer")
 public class CustomerController {
+    @Autowired
+    private CustomerService customerService;
 
+    @GetMapping("/all")
+    public ResponseEntity<List<Customer>>allCustomers(){
+            return ResponseEntity.ok(customerService.allCustomers());
+    }
 }
 
