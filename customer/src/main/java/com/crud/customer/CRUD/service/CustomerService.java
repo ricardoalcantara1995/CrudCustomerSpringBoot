ebf0bf3335ce34ca63cbfa7d701f950af64a050a
Ricardo Alcantara <alcantararicardo04@gmail.com>
2024-10-01 19:34:54 -0500

    CustomerService: agregando el metodo allCustomers retorna todos los clientes en una lista

--- customer/src/main/java/com/crud/customer/CRUD/service/CustomerService.java
+++ customer/src/main/java/com/crud/customer/CRUD/service/CustomerService.java
@@ -1,4 +1,19 @@
 package com.crud.customer.CRUD.service;
 
+import com.crud.customer.CRUD.dto.CustomerDTO;
+import com.crud.customer.CRUD.model.Customer;
+import com.crud.customer.CRUD.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CustomerService {
+    @Autowired
+    CustomerRepository customerRepository;
+
+    public List<Customer>allCustomers(){
+        return customerRepository.findAll();
+    }
 }
