ec792bf330e06d768429183f8d52f7ec5fd11ea2
Ricardo Alcantara <alcantararicardo04@gmail.com>
2024-10-02 17:15:38 -0500

    CustomerService: firmando el metodo con ResourceNotFoundException en caso no encontrar un user

--- customer/src/main/java/com/crud/customer/CRUD/service/CustomerService.java
+++ customer/src/main/java/com/crud/customer/CRUD/service/CustomerService.java
@@ -3,6 +3,7 @@ package com.crud.customer.CRUD.service;
 import com.crud.customer.CRUD.dto.CustomerDTO;
 import com.crud.customer.CRUD.model.Customer;
 import com.crud.customer.CRUD.repository.CustomerRepository;
+import com.crud.customer.GLOBAL.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,8 +17,9 @@ public class CustomerService {
     public List<Customer>allCustomers(){
         return customerRepository.findAll();
     }
-    public Customer oneCustomer(Long id){
-        return customerRepository.findById(id).get();
+    public Customer oneCustomer(Long id) throws ResourceNotFoundException {
+        return customerRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Customer not found or not exist"));
     }
     public Customer saveCustomer(CustomerDTO dto){
         Customer customer = new Customer(dto.getName(), dto.getLastName(), dto.getEmail(),dto.getPhone());
