cd8aa738197acb1ba3e48775c04e26a855d3271e
Ricardo Alcantara <alcantararicardo04@gmail.com>
2024-10-02 11:10:55 -0500

    CustomerController: Se agrega metodo saveCustomer y se prueba en postmant teniendo exito

--- customer/src/main/java/com/crud/customer/CRUD/controller/CustomerController.java
+++ customer/src/main/java/com/crud/customer/CRUD/controller/CustomerController.java
@@ -1,15 +1,14 @@
 package com.crud.customer.CRUD.controller;
 
+import com.crud.customer.CRUD.dto.CustomerDTO;
 import com.crud.customer.CRUD.model.Customer;
 import com.crud.customer.CRUD.service.CustomerService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.http.HttpResponse;
 import java.util.List;
@@ -29,5 +28,10 @@ public class CustomerController {
     public ResponseEntity<Customer> oneCustomer(@PathVariable("id") Long id){
         return ResponseEntity.ok(customerService.oneCustomer(id));
     }
+    @PostMapping("/save")
+    public ResponseEntity<Customer> saveCustomer(@RequestBody CustomerDTO dto){
+        return ResponseEntity.ok(customerService.saveCustomer(dto));
+    }
+
 
 }
