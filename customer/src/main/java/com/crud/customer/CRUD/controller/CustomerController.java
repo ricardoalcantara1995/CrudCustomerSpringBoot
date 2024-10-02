7a7fbfa6add1ce674591de0589f26462a372b0e8
Ricardo Alcantara <alcantararicardo04@gmail.com>
2024-10-02 10:24:21 -0500

    CustomerController: Se agrega metodo oneCustomer para obtener un cliente en concreto, se valida funcionamiento con postman

--- customer/src/main/java/com/crud/customer/CRUD/controller/CustomerController.java
+++ customer/src/main/java/com/crud/customer/CRUD/controller/CustomerController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +24,10 @@ public class CustomerController {
     public ResponseEntity<List<Customer>>allCustomers(){
             return ResponseEntity.ok(customerService.allCustomers());
     }
-}
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Customer> oneCustomer(@PathVariable("id") Long id){
+        return ResponseEntity.ok(customerService.oneCustomer(id));
+    }
+
+}
