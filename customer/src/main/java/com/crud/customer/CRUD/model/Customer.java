2b30d4899ba9f7490f913a9a3fbce3de37cf866a
Ricardo Alcantara <alcantararicardo04@gmail.com>
2024-10-01 19:11:45 -0500

    Customer.java: agregando atributos y anotaciones

--- customer/src/main/java/com/crud/customer/CRUD/model/Customer.java
+++ customer/src/main/java/com/crud/customer/CRUD/model/Customer.java
@@ -1,4 +1,26 @@
 package com.crud.customer.CRUD.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "customer")
 public class Customer {
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(unique = true, name = "nae")
+    private String name;
+    @Column(unique = true, name = "lastname")
+    private String lastName;
+    @Column(unique = true, name = "email")
+    private String email;
+    @Column(unique = true, name = "phone")
+    private String phone;
 }
