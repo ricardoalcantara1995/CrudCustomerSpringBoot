b481c15305a281307c9655009aec941323dd8b62
Ricardo Alcantara <alcantararicardo04@gmail.com>
2024-10-03 05:52:44 -0500

    GlobalException: comenzando creacion de controlador de errores

---
+++ customer/src/main/java/com/crud/customer/GLOBAL/exceptions/GlobalException.java
@@ -0,0 +1,10 @@
+package com.crud.customer.GLOBAL.exceptions;
+
+import com.crud.customer.GLOBAL.dto.MessageDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalException {
+    
+}
