c3afcdc7d1356ebbf9bba9e0f049d5fcf8475182
Ricardo Alcantara <alcantararicardo04@gmail.com>
2024-10-03 07:21:12 -0500

    customerCrud.sql: Se coloca como NOT NULL campo phone

--- customerCrud.sql
+++ customerCrud.sql
@@ -8,7 +8,7 @@ CREATE TABLE customer (
   name VARCHAR(100) NOT NULL,
   lastname VARCHAR(100) NOT NULL,
   email VARCHAR(100) UNIQUE,
-  phone CHAR(15) 
+  phone CHAR(15) NOT NULL
 );
 --generando insert para la tabla 
 
