bba2af6433a71da4aca84d4c5e9e7d1143491862
Ricardo Alcantara <alcantararicardo04@gmail.com>
2024-10-02 19:29:42 -0500

    MessageDTO: agregando atributos , contructores y geters y seters

--- customer/src/main/java/com/crud/customer/GLOBAL/dto/MessageDTO.java
+++ customer/src/main/java/com/crud/customer/GLOBAL/dto/MessageDTO.java
@@ -1,4 +1,13 @@
 package com.crud.customer.GLOBAL.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MessageDTO {
+    private HttpStatus status;
+    private String message;
 }
