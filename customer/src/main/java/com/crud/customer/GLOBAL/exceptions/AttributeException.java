ef798c8472836e3814bf1a6b8cef77584902296f
Ricardo Alcantara <alcantararicardo04@gmail.com>
2024-10-03 06:03:37 -0500

    AttributeException: agregando clase para manejar Exception de duplicidad de correo

---
+++ customer/src/main/java/com/crud/customer/GLOBAL/exceptions/AttributeException.java
@@ -0,0 +1,17 @@
+package com.crud.customer.GLOBAL.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class AttributeException extends Exception{
+    public AttributeException (String message){
+        super(message);
+    }
+}
