f433ebfe292a910e5b0a6f52a444ebe4dfb6620c
Ricardo Alcantara <alcantararicardo04@gmail.com>
2024-10-03 08:33:07 -0500

    CustomerDTO: Se aplican valicion @NotBlank para atributos

--- customer/src/main/java/com/crud/customer/CRUD/dto/CustomerDTO.java
+++ customer/src/main/java/com/crud/customer/CRUD/dto/CustomerDTO.java
@@ -3,6 +3,7 @@ package com.crud.customer.CRUD.dto;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,11 +14,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CustomerDTO {
     @Column(name = "name")
+    @NotBlank(message = "name is mandatory")
     private String name;
     @Column(name = "lastname")
+    @NotBlank(message = "lastName is mandatory")
     private String lastName;
+    @NotBlank(message = "email is mandatory")
     @Column(unique = true, name = "email")
     private String email;
+    @NotBlank(message = "phone is mandatory")
     @Column(name = "phone")
     private String phone;
 }
