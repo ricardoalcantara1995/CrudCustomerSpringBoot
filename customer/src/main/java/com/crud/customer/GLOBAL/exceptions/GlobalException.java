eab952a0b304019b99c73001758a715a3190373b
Ricardo Alcantara <alcantararicardo04@gmail.com>
2024-10-03 05:58:16 -0500

    GlobalException: creando metodo throwNotFoundException para que cuando ocurra una exception de tipo not found sea leida por este metodo

--- customer/src/main/java/com/crud/customer/GLOBAL/exceptions/GlobalException.java
+++ customer/src/main/java/com/crud/customer/GLOBAL/exceptions/GlobalException.java
@@ -1,10 +1,16 @@
 package com.crud.customer.GLOBAL.exceptions;
 
 import com.crud.customer.GLOBAL.dto.MessageDTO;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalException {
-    
+    @ExceptionHandler(ResourceNotFoundException.class)
+    ResponseEntity<MessageDTO>throwNotFoundException(ResourceNotFoundException exception){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new MessageDTO(HttpStatus.NOT_FOUND,exception.getMessage()));
+    }
 }
