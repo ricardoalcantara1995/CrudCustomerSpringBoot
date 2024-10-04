c33ce5f3acee51e6a2b3204906089675b60fdb0a
Ricardo Alcantara <alcantararicardo04@gmail.com>
2024-10-04 00:08:02 -0500

    GlobalException: Se agrega control de error validateException para retornar mensaje en caso no pase @Valid

--- customer/src/main/java/com/crud/customer/GLOBAL/exceptions/GlobalException.java
+++ customer/src/main/java/com/crud/customer/GLOBAL/exceptions/GlobalException.java
@@ -4,24 +4,37 @@ import com.crud.customer.GLOBAL.dto.MessageDTO;
 import org.hibernate.internal.ExceptionConverterImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestControllerAdvice
 public class GlobalException {
     @ExceptionHandler(ResourceNotFoundException.class)
-    ResponseEntity<MessageDTO>throwNotFoundException(ResourceNotFoundException exception){
+    public ResponseEntity<MessageDTO>throwNotFoundException(ResourceNotFoundException exception){
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
                     .body(new MessageDTO(HttpStatus.NOT_FOUND,exception.getMessage()));
     }
     @ExceptionHandler(AttributeException.class)
-    ResponseEntity<MessageDTO>throwAttributeException(AttributeException exception){
+    public ResponseEntity<MessageDTO>throwAttributeException(AttributeException exception){
         return ResponseEntity.badRequest()
                 .body(new MessageDTO(HttpStatus.BAD_REQUEST,exception.getMessage()));
     }
     @ExceptionHandler(Exception.class)
-    ResponseEntity<MessageDTO>generalException(Exception exception){
+    public ResponseEntity<MessageDTO>generalException(Exception exception){
         return ResponseEntity.internalServerError()
                 .body(new MessageDTO(HttpStatus.INTERNAL_SERVER_ERROR,exception.getMessage()));
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<MessageDTO>validateException(MethodArgumentNotValidException exception){
+        List<String>errorMessages = new ArrayList<>();
+        exception.getBindingResult().getAllErrors().forEach((error) -> {
+            errorMessages.add(error.getDefaultMessage());
+        });
+        return ResponseEntity.badRequest()
+                .body(new MessageDTO(HttpStatus.BAD_REQUEST,errorMessages.toString()));
     }
 }
