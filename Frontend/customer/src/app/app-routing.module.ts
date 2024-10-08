7f01ed75d5acd99694938cb90ac924ac7fce8497
Ricardo Alcantara <alcantararicardo04@gmail.com>
2024-10-08 18:31:04 -0500

    app-routing.module.ts: Se agregaron las rutas a los componentes

--- Frontend/customer/src/app/app-routing.module.ts
+++ Frontend/customer/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListComponent } from './customer/list.component';
+import { DetailComponent } from './customer/detail.component';
+import { UpdateComponent } from './customer/update.component';
+import { CreateComponent } from './customer/create.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component: ListComponent},
+  {path:'detail',component: DetailComponent},
+  {path:'update',component: UpdateComponent},
+  {path:'create',component: CreateComponent},
+  {path:'**',redirectTo:'', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
