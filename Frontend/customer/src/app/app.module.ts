0e470d0b29879c5f4e66bdecc3b018599fffe9c7
Ricardo Alcantara <alcantararicardo04@gmail.com>
2024-10-08 16:16:50 -0500

    Agregando paquetes de terceros sweetalert2 - animations - toastr

--- Frontend/customer/src/app/app.module.ts
+++ Frontend/customer/src/app/app.module.ts
@@ -4,13 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//Paquetes de terceros
+
+//SweetAlert2
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
+//animations
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//toastr
+import { ToastrModule } from 'ngx-toastr';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SweetAlert2Module.forRoot(),
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
