39c5311094b1df54d512a413d1424219b1426964
Ricardo Alcantara <alcantararicardo04@gmail.com>
2024-10-12 11:36:10 -0500

    service.ts: se agrega metodo para listar a todos los clientes

--- Frontend/customer/src/app/services/customer.service.ts
+++ Frontend/customer/src/app/services/customer.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Customer } from '../model/customer';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
-
-  constructor() { }
+  customerURL =  "http://192.168.18.12:8080/customer/"+ "/all"
+  constructor(private httpClient:HttpClient ) { }
+  public list(): Observable<Customer[]>{
+    return this.httpClient.get<Customer[]>(this.customerURL);
+  }
 }
+  //http://192.168.18.12:8080/customer/
