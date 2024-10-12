1b7913a846248f59bd5efc046e5055083ea2672e
Ricardo Alcantara <alcantararicardo04@gmail.com>
2024-10-11 21:19:31 -0500

    customer.ts: agregando atributos y constructoer de customer

--- Frontend/customer/src/app/model/customer.ts
+++ Frontend/customer/src/app/model/customer.ts
@@ -1,2 +1,14 @@
 export class Customer {
+    id: number;
+    name: string;
+    lastName:string;
+    email:string;
+    phone:string;
+
+    constructor(name:string,lastName:string ,email:string,phone:string){
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+    }
 }
